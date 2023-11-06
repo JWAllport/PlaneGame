a55b18e10d75238c3bb8fc98f07f3d2022fd025c
Jake Allport <jake.w.allport@gmail.com>
2023-11-06 11:20:36 -0500

    initial commit

---
+++ Assets/Challenge 1/Scripts/Explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    GameObject Plane;
+    public Explosion()
+    {
+
+        void OnCollisionEnter() {
+
+            Destroy(Plane);
+
+
+        }
+    }
+}
---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = plane.transform.position;
+        transform.rotation = plane.transform.rotation;
+      
+
+    }
+}
